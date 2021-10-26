c10ae543de201940601fe31e2695db95f19c9c9b
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-10-26 07:07:17 -0300

    Inserir Dados no Banco de Dados com o método prepare e query().

--- crud-pdo.php
+++ crud-pdo.php
@@ -6,3 +6,20 @@ try{
 }catch(Exception $e){
     echo "Erro genérico: ".$e->getMessage();;
 }
+//-------------------------------------INSERT--------------------------------------------------------//
+//1°Forma
+//Inserir Valores no Banco de Dados
+$res = $pdo->prepare("INSERT INTO pessoa(nome,telefone,email)
+    VALUES(:n,:t,:e)");
+
+//Valores
+$res->bindValue(":n","Roberta");
+$res->bindValue(":t","987643461");
+$res->bindValue(":e","roberta@gmail.com");
+$res->execute(); 
+
+
+//2°Forma:
+//$pdo->query("INSERT INTO pessoa(nome,telefone,email)
+//VALUES ('Paulo','906856536','paulo@gmail')");
+?>
