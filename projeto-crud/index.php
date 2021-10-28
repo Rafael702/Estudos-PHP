59b74be55e20cdf005c8ee4cb0d2feb4be1df27d
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-10-28 06:04:36 -0300

    Criar Tabela Cadastro do Projeto CRUD.

---
+++ projeto-crud/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CADASTRO PESSOA</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <section id="esquerda">
+    <form>
+    <h2>CADASTRAR PESSOA</h2>
+    <label for="nome">NOME</label>
+    <input type="text" name="nome" id="nome">
+    <label for="telefone">TELEFONE</label>
+    <input type="text" name="telefone" id="telefone">
+    <label for="email">EMAIL</label>
+    <input type="email" name="email" id="email">
+    <input type="submit" value="Cadastrar">
+    </form>
+    </section>
+    <section id="direita">
+    <table>
+        <tr id="titulo">
+            <td>NOME</td>
+            <td>TELEFONE</td>
+            <td colspan="2">EMAIL</td>
+        </tr>
+        <tr>
+            <td>Maria</td>
+            <td>0000</td>
+            <td>Maria@maria</td>
+            <td><a href="">Editar</a><a href="">Excluir</a></td>
+        </tr>
+    </table>
+    </section>    
+</body>
+</html>
