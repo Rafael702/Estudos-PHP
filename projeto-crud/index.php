79898f1cb35a1dab7ba98a03e2fb9c290c3059d3
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-15 11:48:52 -0300

    Incluir o método excluir.

--- projeto-crud/index.php
+++ projeto-crud/index.php
@@ -60,7 +60,10 @@ $p = new Pessoa("crudpdo", "localhost", "root", "");
                         }
                     }
             ?>
-                    <td><a href="">Editar</a><a href="">Excluir</a></td>
+                    <td>
+                        <a href="">Editar</a>
+                        <a href="index.php?id=<?php echo $dados[$i]['id']; ?>">Excluir</a>
+                    </td>
             <?php
                     echo "</tr>";
                 }
@@ -75,3 +78,11 @@ $p = new Pessoa("crudpdo", "localhost", "root", "");
 </body>
 
 </html>
+
+<?php
+if (isset($_GET['id'])) {
+    $id_pessoa = addslashes($_GET['id']);
+    $p->excluirPessoa($id_pessoa);
+    header("location: index.php");
+}
+?>
